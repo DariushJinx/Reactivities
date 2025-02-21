09d49ccd602d33e3408bb9458fb25446fd7d5501
dariushkh <www.dariushkhazaei629@gmail.com>
2025-02-22 01:37:11 +0330

    Add project files.

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain;
+
+public class Activity
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+
+    // location props
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+}
